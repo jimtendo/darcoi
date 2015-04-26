ff687a9a84724df1b306c385f88f60c672aa64ba
Jim Hamill <jim@optimising.com.au>
2015-04-26 13:05:52 +1000

    Blue effect with comments

--- effects/blue.c
+++ effects/blue.c
@@ -1,9 +1,13 @@
+// If Pixel's red channel, green channel and blue channel is more than 0.1...
 if (pixel.r > 0.1 && pixel.g > 0.1 && pixel.b > 0.1) {
+  
+    // Make the red and green channel empty, but make the blue channel the average of red, green and blue
     pixel.r = 0.0;
     pixel.g = 0.0;
-    pixel.b = (pixel.r + pixel.g + pixel.b) / 3.0;
+    pixel.b = (pixel.r + pixel.g + pixel.b) / 3.0; // (Adding and then dividing by three is the average of rgb)
 }
 
+// Otherwise make the pixel black
 else {
     pixel.r = 0.0;
     pixel.g = 0.0;
